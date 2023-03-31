709af6d3b37b2cac667f59ddb35ab2a0cb110006
Sahad M <sahadm040@gmail.com>
2023-03-31 11:40:01 +0530

    hddjl

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
 import Contact from "../components/contact";
 import SNavbar from "../components/navbar";
 import Sabout from "../components/sabout";
